025692a11b07d742f1a838f9b8a038dd6d10ec87
Yas <yasbas@gmail.com>
2020-10-14 16:55:31 +0300

    Moved Roles seeding into a migration.

---
+++ database/migrations/2020_10_14_104000_create_default_roles.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Spatie\Permission\Models\Role;
+
+class CreateDefaultRoles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'user']);
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            RolesSeeder::class,
             UsersSeeder::class,
             TicketsSeeder::class,
         ]);
